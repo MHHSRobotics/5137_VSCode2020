80c2b4c69f5056aa32be493bc57059f325d74355
BrianMere <brianmere@cox.net>
2020-01-22 16:14:49 -0800

    Daily Commit.

--- 2020JavaCode/src/main/java/frc/robot/subsystems/DriveBase_Subsystem.java
+++ 2020JavaCode/src/main/java/frc/robot/subsystems/DriveBase_Subsystem.java
@@ -2,21 +2,24 @@ package frc.robot.subsystems;
 
 //New API may not need to import dependable commands
 import frc.robot.RobotContainer; //Import Timed Robot methods (from overall robot)
-//import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX; //Check which motor controller would work better (WPI Talons v. Talons)
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase; //Import Subsystem Class (*new this year*)
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive; //Import DifferentialDrive (a way to have an arcade drive)
 
 public class DriveBase_Subsystem extends SubsystemBase {
+	SpeedControllerGroup m_leftDrive = RobotContainer.m_leftDrive;
+	SpeedControllerGroup m_rightDrive = RobotContainer.m_rightDrive;
+	DifferentialDrive BMoneysDifferentialDrive = RobotContainer.BMoneysDriveBase;
 
 	public void DriveBase_Subsystem() {
-		SpeedControllerGroup leftDrive = RobotContainer.m_leftDrive;
-		SpeedControllerGroup rightDrive = RobotContainer.m_rightDrive;
-		DifferentialDrive BMoneysDifferentialDrive = RobotContainer.BMoneysDriveBase;
+
 		
 	}
 
@@ -26,7 +29,7 @@ public class DriveBase_Subsystem extends SubsystemBase {
 	}
 
 	public void initDefaultCommand() {
-		//CommandScheduler.getInstance().setDefaultCommand(driveBase_Subsystem, new ArcadeDrive());
+		setDefaultCommand(new ArcadeDrive());
 	}
 
 	public double adjustJoystickValue(double joystick, double deadZone) {
@@ -41,5 +44,22 @@ public class DriveBase_Subsystem extends SubsystemBase {
 		//	to run scaled to the deadZone
 	}
 
-	
+	public void rampArcadeDrive(Joystick XBoxController) {
+		double driveValue = XBoxController.getRawAxis(Constants.LYStickAxisPort);
+		double turnValue = XBoxController.getRawAxis(Constants.RXStickAxisPort);
+
+		
+	}
+
+	public void drivePivot(double speed) { //TODO may need to make this negative
+		BMoneysDifferentialDrive.arcadeDrive(0, speed);
+	}
+
+	public void driveStraight(double speed) {
+		BMoneysDifferentialDrive.arcadeDrive(speed,0);
+	}
+
+	public void stop() {
+		BMoneysDifferentialDrive.arcadeDrive(0,0);
+	}
 }
