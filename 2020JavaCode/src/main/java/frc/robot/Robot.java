5214c727022cfa992b7789a7c5c741bdad39da7f
BrianMere <brianmere@cox.net>
2020-01-23 12:23:12 -0800

    Added code regarding motor safety check issue.

--- 2020JavaCode/src/main/java/frc/robot/Robot.java
+++ 2020JavaCode/src/main/java/frc/robot/Robot.java
@@ -50,6 +50,9 @@ public class Robot extends TimedRobot {
     Let's head over to it right now to see what it's doing.*/
     ExistingRobot = new RobotContainer();
 
+    /*Possibly add inverted method for all motors (TODO need to test!!!)
+    ExistingRobot.m_leftDrive.setInverted(true);
+    ExistingRobot.m_rightDrive.setInverted(true); */
     //Default template stuff
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
