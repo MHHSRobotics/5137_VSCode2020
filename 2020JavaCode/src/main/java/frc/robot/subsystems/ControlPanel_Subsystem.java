331f3466d5067a851c2789d1b377110e609b0ce0
BrianMere <brianmere@cox.net>
2020-01-26 00:16:32 -0800

    Added Control Panel Items.

--- 2020JavaCode/src/main/java/frc/robot/subsystems/ControlPanel_Subsystem.java
+++ 2020JavaCode/src/main/java/frc/robot/subsystems/ControlPanel_Subsystem.java
@@ -4,16 +4,54 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ControlPanel_Subsystem extends SubsystemBase {
     WPI_VictorSPX controlPanelVictor = RobotContainer.controlPanelVictor;
 
+    SmartDashboard smartDashboard;
+
     public ControlPanel_Subsystem() {
+        controlPanelVictor = RobotContainer.controlPanelVictor;
+        smartDashboard = RobotContainer.smartDashboard;
+    }
+
+    public void controlPanelPeriodic() {
 
     }
 
-    public void convertGameSignal() {
+    public char adjustColorSignal() {
 
+        String gameData;
+        gameData = DriverStation.getInstance().getGameSpecificMessage();
+        if (gameData.length() > 0) {
+            switch (gameData.charAt(0)) {
+            case 'B':
+                // Blue case code
+
+                break;
+                //smartDashboard.setImage();
+            case 'G':
+                // Green case code
+
+                break;
+            case 'R':
+                // Red case code
+
+                break;
+            case 'Y':
+                // Yellow case code
+
+                break;
+            default:
+                // This is corrupt data
+                break;
+            }
+        } else {
+            // Code for no data received yet
+        }
+        return ' ';
     }
+
 }
-
