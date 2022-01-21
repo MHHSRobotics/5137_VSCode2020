57fc98853c89fef7dd7339f7f1f748c06d7a1a45
SahanaRa <sahananr13@gmail.com>
2022-01-20 16:55:07 -0800

    Attempting to create team 2's branch

--- 2020JavaCode/src/main/java/frc/robot/subsystems/Intake_Subsystem.java
+++ 2020JavaCode/src/main/java/frc/robot/subsystems/Intake_Subsystem.java
@@ -72,26 +72,6 @@ public class Intake_Subsystem extends SubsystemBase {
         }
     }
 
-    //All code below is outdated...
-    /*
-    public double getAmmoCount() {
-        //smartDashboard.putNumber("Title", double number);
-        refreshAmmoCount();
-        return SmartDashboard.getNumber("Ball Count", errorDefaultValue);
-    }
-
-    public void refreshAmmoCount() {
-        boolean catchError = false;
-
-        if (SmartDashboard.getBoolean("Add 1 Ball", catchError) == true) {
-            SmartDashboard.putNumber("Ball Count", (SmartDashboard.getNumber("Ball Count", errorDefaultValue) + 1.0));
-            SmartDashboard.putBoolean("Add 1 Ball", false);
-        }
-        else if (SmartDashboard.getBoolean("Subtract 1 Ball", catchError) == true) {
-            SmartDashboard.putNumber("Ball Count", (SmartDashboard.getNumber("Ball Count", errorDefaultValue) - 1.0));
-            SmartDashboard.putBoolean("Subtract 1 Ball", false);
-        }
-    } */
 
     public void endIntake() {
         intakeVictor.set(0);
